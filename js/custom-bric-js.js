4315d3256cfb166bf2913fec58ec359a58615894
AmosFitness <8171481+wyk111wyk@users.noreply.github.com>
2024-11-05 14:42:10 +0800

    添加新应用
    - 网站改版和更新内容

---
+++ js/custom-bric-js.js
@@ -0,0 +1,15 @@
+/* This JS contains values set by the User */
+
+$(document).ready(function()
+{	
+	// Variables written (require % at beginning and end)
+	// popover-title
+	// popover-message
+	// popover-bg-color
+	// popover-btn-label
+	
+	// Add Model To Page
+	$('body').append('<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content" style="background-color:#FFFFFF;"><div class="modal-header"><h5 class="modal-title" id="exampleModalLongTitle">Title</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><h1>Hello World</h1></div><div class="modal-footer"><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>');
+});
+
+
